1b283e7a44ae3b32379a90c2e81d3f8149778abe
xuliang <xuliang@joyme.sg>
2020-12-24 14:30:28 +0800

    线程池处理类增加描述

--- src/main/java/com/thread/con/rest/ThreadController.java
+++ src/main/java/com/thread/con/rest/ThreadController.java
@@ -15,10 +15,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @RestController
-@RequestMapping("thread-queue")
+@RequestMapping("thread")
 public class ThreadController {
 
 
